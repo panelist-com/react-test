adce9465fb1c4958d2b713371bed7db8c55bb4b0
Frazer Irving <frazer.irving@team.panelist.com>
2022-03-03 16:26:55 +1100

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [aliens, setAliens] = useState(0);
+  const [robots, setRobots] = useState(0);
+  const [countdown, setCountdown] = useState(60);
+
+  setTimeout(() => countdown && setCountdown(countdown - 1), 1000);
+
+  const reset = () => {
+    setCountdown(60);
+    setRobots(0);
+    setAliens(0);
+  };
+
+  const winner = aliens > robots ? "Aliens" : robots > aliens ? "Robots" : "";
+  const gameOver = countdown <= 0;
+  let message = gameOver
+    ? winner
+      ? `${winner} won`
+      : `nobody won`
+    : (winner ? `${winner} are winning` : "nobody is winning") +
+      ` (${countdown} seconds remaining)`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row justify-content-md-center>
+          <Col className="coll-2">
+            <h3>{message}</h3>
+            <button onClick={reset}>New Game</button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>{robots} robots</h3>
+            <button onClick={() => setRobots(robots + 1)}>Add Robot</button>
+          </Col>
+          <Col>
+            <h3>{aliens} aliens</h3>
+            <button onClick={() => setAliens(aliens + 1)}>Add Alien</button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="2"></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
